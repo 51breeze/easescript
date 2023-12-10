528988ada1323ae6e75434e18d25ad88cebba29a
Jun Ye <664371281@qq.com>
2023-12-10 16:02:40 +0800

    async

--- test/index.js
+++ test/index.js
@@ -19,26 +19,37 @@ specs.forEach(file=>require(path.join(root,file)));
 
 describe('compile file', function() {
     const creator = new compiler.Creator();
+    let compilation = null;
+    let errors = [];
+    let module = null;
+    beforeAll(async function() {
+        compilation = await creator.startByFile('./Test.es');
+        errors = compilation.compiler.errors;
+    });
+
+    afterAll(()=>{
+        errors.forEach( item=>{
+            if( item.kind == 0 ){
+                fail( item.toString() )
+            }
+        });
+        compilation = null;
+    })
+
     it('should compile success and build', async function() {
-        const compilation = await creator.startByFile("./Test.es")
-        const errors = compilation.compiler.errors;
+       
         expect('Expected 0 errors').toContain( errors.length );
-        if( errors.length===0 ){
-            const jsxElement = compilation.getReference('jsxElement',  compilation.getModuleById('Test') );
-            var stack = jsxElement.body.body[1].declarations[0].init
-            expect('JSXElement').toEqual( stack.node.type );
-            expect('JSXOpeningElement').toEqual( stack.openingElement.node.type );
-            expect(true).toEqual( stack.openingElement.name.isJSXIdentifier );
-            expect('JSXClosingElement').toEqual( stack.closingElement.node.type );
-            expect( true ).toEqual( Array.isArray( stack.children ) );
-            stack = jsxElement.body.body[2].declarations[0].init;
-            expect([]).toEqual( stack.openingElement.attributes );
-            expect('div').toEqual( stack.openingElement.name.value() );
-        }else{
-            // errors.forEach((error)=>{
-            //     fail(error.toString());
-            // });
-        }
+        const jsxElement = compilation.getReference('jsxElement',  compilation.getModuleById('Test') );
+        var stack = jsxElement.body.body[1].declarations[0].init
+        expect('JSXElement').toEqual( stack.node.type );
+        expect('JSXOpeningElement').toEqual( stack.openingElement.node.type );
+        expect(true).toEqual( stack.openingElement.name.isJSXIdentifier );
+        expect('JSXClosingElement').toEqual( stack.closingElement.node.type );
+        expect( true ).toEqual( Array.isArray( stack.children ) );
+        stack = jsxElement.body.body[2].declarations[0].init;
+        expect([]).toEqual( stack.openingElement.attributes );
+        expect('div').toEqual( stack.openingElement.name.value() );
+        
     })
     
 });
