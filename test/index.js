952e5e6378569ce6bcc38fc2a5875610d90fb7ca
51breeze <664371281@qq.com>
2024-04-17 19:41:51 +0800

    fix

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const compiler = require("./compiler");
 const root = path.join(__dirname,'./specs');
-
+const Glob = require('../lib/core/Glob')
 const specs = fs.readdirSync( root );
 specs.forEach(file=>require(path.join(root,file)));
 
@@ -85,6 +85,48 @@ describe('compile file', function() {
 
     
 // });
+
+
+
+describe('Glob', function() {
+    var globInstance = null;
+    beforeAll(async function() {
+        globInstance = new Glob()
+    });
+
+    it('test', function() {
+        globInstance.addRule('***', 'test/{...}/{basename}')
+        globInstance.addRule('**/*.json', 'json/{...}/{basename}')
+        globInstance.addRule('test/**/*.es', 'test/{...}/../{basename}')
+        globInstance.addRule('test/**/*.*', 'test/{...}/../all/{basename}')
+        globInstance.addRule('test/dir/**/*.*', 'test/{0}/{1}-{2}/{basename}')
+        globInstance.addRule('**/api/model/**/*.*', 'test/build/server/model/{...globs[0]}/{globs[1][1]}/{basename}')
+        globInstance.addRule('**/apis/*/model/**/*.*', 'test/apis/server/model/{...globs[0]}/{globs[1]}/{globs[2]}/{basename}')
+        globInstance.addRule('**/apis/*/*/*.js', 'test/js/{globs[1]}-{globs[2]}{basename}')
+        globInstance.addRule('com/**/test', 'com/{...}/test/ok')
+        globInstance.addRule('com/*/test/api/*', 'com/{...}/test/api/{1}')
+        globInstance.addRule('com/*/test/api/**', 'coms/{0}/test/api/{globs[1]}')
+        globInstance.addRule('com/**/apis/**', 'comss/{-2}/{0}/test/api/{globs[1]}')
+
+        expect('test/src/api/http/Index.es').toEqual( globInstance.dest('src/api/http/Index.es') )
+        expect('test/src/api/Index.es').toEqual( globInstance.dest('test/src/api/http/Index.es') )
+        expect('test/src/api/all/Index.js').toEqual( globInstance.dest('test/src/api/http/Index.js') )
+        expect('test/build/server/model/test/com/test/Category.es').toEqual( globInstance.dest('test/com/api/model/category/test/Category.es') )
+        expect('json/src/assets/front/met.json').toEqual( globInstance.dest('src/assets/front/met.json') )
+        expect('test/dev/src-controller/http.es').toEqual( globInstance.dest('test/dir/dev/src/controller/http.es') )
+
+        expect('test/apis/server/model/test/prod/person/test/com/Category.es').toEqual( globInstance.dest('test/prod/apis/person/model/test/com/Category.es') )
+        expect('test/js/hash-prodindex.js').toEqual( globInstance.dest('test/prod/apis/hash/prod/index.js') )
+
+        expect('com.test.http.test.ok').toEqual( globInstance.dest('com/test/http/test', {delimiter:'.'}) )
+        expect('com.http.dev.test.api.dev').toEqual( globInstance.dest('com/http/test/api/dev', {delimiter:'.'}) )
+
+        expect('coms.http.test.api.dev.cc.name').toEqual( globInstance.dest('com/http/test/api/dev/cc/name', {delimiter:'.'}) )
+        expect('comss.cc.http.test.api.dev.cc.name').toEqual( globInstance.dest('com/http/test/apis/dev/cc/name', {delimiter:'.'}) )
+
+    })
+
+})
 
 
 
