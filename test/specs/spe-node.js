05d6e07d62d52a393d7eb6dde5397c8275430044
yejun123 <664371281@qq.com>
2024-06-15 15:32:59 +0800

    fix

--- test/specs/spe-node.js
+++ test/specs/spe-node.js
@@ -36,24 +36,29 @@ describe('test components Child', function() {
          const start = compilation.stack;
          let expression = start.childrenStack[0];
          let def = expression.specifiers[0].toDefinition();
-         expect('(import refers) assert: <T = any>(value: T, message?: string | Error)=>T').toEqual(def.expre)
+         if(Array.isArray(def))def = def[0];
+         expect(def.expre).toContain('(alias) function assert<T = any>(value: T, message?: string | Error): T')
          expect('assert.d.es').toEqual( path.basename(def.file) )
 
          def = expression.specifiers[1].toDefinition();
-         expect('(import refers) class assert.AssertionError').toEqual(def.expre)
+         if(Array.isArray(def))def = def[0];
+         expect(def.expre ).toContain('(alias) class assert.AssertionError')
          expect('assert.d.es').toEqual( path.basename(def.file) )
 
          def = expression.specifiers[2].toDefinition();
-         expect('(import refers) exit: ()=>void').toEqual(def.expre)
+         if(Array.isArray(def))def = def[0];
+         expect(def.expre).toContain('(alias) function exit(): void')
          def =  expression.specifiers[2].description();
-
+         
          expect('ImportSpecifier').toEqual( def.toString() )
 
          def = def.description();
          expect('process.d.es').toEqual( path.basename(def.file) )
 
          def = expression.specifiers[3].toDefinition();
-         expect('(import refers) child: []').toEqual(def.expre)
+         if(Array.isArray(def))def = def[0];
+         expect(def.expre).toContain('import child')
+         expect(def.expre).toContain('(local const) child:[]')
 
 
          expression = start.childrenStack[1].expression
@@ -65,13 +70,20 @@ describe('test components Child', function() {
          expect('assert.AssertionError').toEqual( expression.type().toString() )
 
          def = start.childrenStack[3].specifiers[0].toDefinition();
-         expect('(import refers) config: {}').toEqual( def.expre )
+         if(Array.isArray(def))def = def[0];
+         expect(def.expre).toContain('import config')
+         expect(def.expre).toContain('(local const) config:{}')
 
          def = start.childrenStack[4].specifiers[0].toDefinition();
-         expect('(import refers) database: string').toEqual( def.expre )
+         if(Array.isArray(def))def = def[0];
+         expect(def.expre).toContain('(local const) database:string')
+         expect(def.expre).toContain('import database')
+
 
          def = start.childrenStack[4].specifiers[1].toDefinition();
-         expect('(import refers) test: (val: string)=>boolean').toEqual( def.expre )
+         if(Array.isArray(def))def = def[0];
+         expect(def.expre).toContain('(alias) function test(val: string): boolean')
+         expect(def.expre).toContain('import test')
 
          def = start.childrenStack[4].specifiers[2].description()
          expect(true).toBeTrue( def.type().isLiteralObjectType )
